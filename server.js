54d7990d63b4b9e124a781536514adaa31ee04c8
lordsainath <lordsainath8@gmail.com>
2026-03-14 16:23:09 +0530

    some changes are made in cors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
+const PORT = process.env.PORT || 5000
 const app = require('./src/app')
 const connectDB = require('./src/config/db')
-const PORT = process.env.PORT || 5000
-const cors = require('cors')
+
+
 
 connectDB();
 
-app.use(cors())
 
 app.listen(PORT, () => {
     console.log('server started on port', PORT)
--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cookieParser = require('cookie-parser')
 const authRouter = require("./routes/auth.routes");
 const app = express();
+const cors = require('cors')
+app.use(cors())
 
 
 app.use(cookieParser())
